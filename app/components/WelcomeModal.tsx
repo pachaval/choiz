1ecdd03a1a6cc4e828a21fda1fe837565f6ee81c
pabloachaval@cookunity.com <pabloachaval@gmail.com>
2025-02-01 00:03:14 -0300

    fix: styles

--- app/components/WelcomeModal.tsx
+++ app/components/WelcomeModal.tsx
@@ -7,16 +7,15 @@ import NextButton from "./NextButton";
 const WelcomeModal: React.FC = () => {
   return (
     <div className="fixed inset-0 flex flex-col">
-      <div className="welcome-bg">
+      <div className="flex-1 overflow-hidden relative bg-[#6042AA] flex items-start">
         <Image
-          className="welcome-bg-img"
+          className="object-cover scale-[1.8] object-[center_-60px]"
           src="/assets/images/welcome.jpeg"
           alt="Welcome Background"
           quality={50}
           priority
           fill
         />
-
         <div className="absolute top-8 left-8 z-10">
           <Image
             src="assets/icons/choiz.svg"
@@ -27,14 +26,15 @@ const WelcomeModal: React.FC = () => {
         </div>
       </div>
 
-      <div className="welcome-bottomsheet">
+      <div className="flex flex-col bg-white rounded-t-3xl shadow-lg p-7 relative -mt-6 min-h-fit max-h-[50vh]">
         <h2 className="text-[#6042AA] text-[26px] font-medium mb-3 mt-2">
           Bienvenido a Choiz
         </h2>
-        <p className="text-[#424B54] text-[16px] mb-7">
+        <p className="text-[#424B54] text-[16px] mb-5">
           Comienza tu tratamiento en tres pasos:
         </p>
         <WelcomeStepList />
+        <div className="flex-grow" />
         <NextButton />
       </div>
     </div>
